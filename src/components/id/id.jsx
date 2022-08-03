533309f261b169703070452259341be928e3ec59
Husniddin <jamoliddinovhusniddin7212@gmail.com>
2022-08-04 04:53:15 +0500

    add new codes to id component

--- src/components/id/id.jsx
+++ src/components/id/id.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
-import './id.scss';
+import "./id.scss";
 
 class Id extends Component {
   render() {
     const { issue } = this.props;
-    return <p>{issue.id}</p>;
+    return (
+      <p>
+        #{issue.id + " opened " + issue.createdDate + "  "} by{" "}
+        <a href={issue.user.avatar_url} className="tooltipLink">
+          <button
+            type="button"
+            className="btn btn-secondary d-block py-3 px-5"
+            data-toggle="tooltip"
+            data-html="true"
+            title={
+              issue.user.username.toUpperCase() +
+              " from " +
+              issue.user.address.city +
+              ", " +
+              issue.user.address.state
+            }
+          >
+            {issue.user.username}
+          </button>
+        </a>
+      </p>
+    );
   }
 }
 
